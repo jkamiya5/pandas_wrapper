227124947fa69ebe0753461500e605fcbfff95ff
kamiya <kamiya@id-s.net>
2017-12-01 00:39:00 +0900

    修正

--- pandas_wrapper.py
+++ pandas_wrapper.py
@@ -28,31 +28,41 @@ class PandasWrapper(object):
   def __init__(self, project_id):
     self.datastore_client = datastore.Client(project_id)
 
-  def dataframe_to_datastore(self, df, datastore_kind_name=None, update_keys=None, over_write=False):
+  def dataframe_to_datastore(self, df, datastore_kind_name=None, update_keys=None, if_exists="skip"):
 
     logger.debug("datastore_kind_name:" + str(datastore_kind_name))
     logger.debug("update_keys:" + str(update_keys))
-
     if datastore_kind_name is None or datastore_kind_name == "":
       return
 
+    count = 0
     for index, row in df.iterrows():
+      result = None
       query = self.datastore_client.query(kind=datastore_kind_name)
       if update_keys is not None and isinstance(update_keys, list):
         for k in update_keys:
           query.add_filter(k, '=', row[k])
 
-      query_iter = query.fetch()
-      result = list(query_iter)
+        query_iter = query.fetch()
+        result = list(query_iter)
+
       task = datastore.Entity(self.datastore_client.key(datastore_kind_name))
 
-      if len(result) > 0:
-        task = result[0]
-        logger.debug("update:" + str(task))
-        if not (over_write):
+      if result is not None and len(result) > 0:
+        if if_exists == "skip":
+          logger.debug("skip:" + str(result))
           continue
+        elif if_exists == "replace":
+          logger.debug("replace:" + str(result))
+          task = result[0]
+          logger.debug("update:" + str(result))
+        elif if_exists == "append":
+          logger.debug("insert:" + str(result))
+        else:
+          logger.debug("args error.")
+          return
       else:
-        logger.debug("insert:" + str(task))
+        logger.debug("insert:" + str(result))
 
       for col_name in df.columns:
         val = None
@@ -65,6 +75,9 @@ class PandasWrapper(object):
 
       logger.debug("task:" + str(task))
       self.datastore_client.put(task)
+      count += 1
+
+    logger.debug("proc done." + "count:" + str(count))
 
   def set_diff_dataframes(self, df, df2, join_keys=None):
 
